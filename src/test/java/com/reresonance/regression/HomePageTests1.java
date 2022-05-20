5d931f954c8ec46564c7f0b8e13dab6c8f673af0
unknown <nikbobade51@gmail.com>
2022-05-20 10:48:11 +0100

    First PUsh my

--- src/test/java/com/reresonance/regression/HomePageTests1.java
+++ src/test/java/com/reresonance/regression/HomePageTests1.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 
 import com.resonance.pagse.HomePageThreeObject;
 
-public class HomePageTests {
+public class HomePageTests1 {
 	
 	@Test
 	public void verifyTitleOfTheHomePage() {
 		HomePageThreeObject home = new HomePageThreeObject();
 		String title=home.getTitle();
-		Assert.assertEquals("Home Page", title);
+		Assert.assertEquals("Testing shastra", title);
 		
 
 	}
