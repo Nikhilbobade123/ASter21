2f9aebfbff123b4f700fb007fd99aa2508157a6e
snehadhan98 <sneha.dhanorkar@gmail.com>
2022-05-24 18:37:11 +0530

    updated version of nikhil

--- src/test/java/com/reresonance/regression/Rohan.java
+++ src/test/java/com/reresonance/regression/Rohan.java
@@ -3,6 +3,8 @@ package com.reresonance.regression;
 public class Rohan {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		String s ="Nikhil Bobade";
+		System.out.println(s);
 	}
 
 }
